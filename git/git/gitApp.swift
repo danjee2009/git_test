4b753b5a3ed138f52dc36369229fd21532580c24
gituser <danjee2009@gmail.com>
2025-07-10 19:34:43 +0900

    xcode file add

---
+++ git/git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by maple on 7/10/25.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
